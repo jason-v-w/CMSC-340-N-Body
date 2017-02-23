04dd67c8e9fe120e05e0169f397cae83a4f0dc3c
Jason Vander Woude <jasonvwoude@gmail.com>
2017-02-22 19:49:15 -0600

    Finish bodies.c and bodies.h

--- bodies.c
+++ bodies.c
@@ -1,51 +1,51 @@
-// 3 dimensional position vector
-typedef struct position {
-  double x, y, z;
-} position;
-
-// 3 dimensional velocity vector
-typedef struct velocity {
-  double x, y, z;
-} velocity;
-
-// 3 dimensional acceleration vector
-typedef struct acceleration {
-  double x, y, z;
-} acceleration;
-
-// 3 dimensional force vector
-typedef struct force {
-  double x, y, z;
-} force;
-
-typedef struct body {
-  double density, mass;
-  position pos;
-  velocity vel;
-} body;
+#include <math.h>
+#include "bodies.h"
 
 
-/*
 
-
-/* Given two bodies, the force on body 1 from body 2 will be returned in a force struct
+/* Given a vector3D, the vector magnitude is returned
+ * 
+ * @author Jason Vander Woude
  */
-force getForce(body bodyOn, body bodyFrom) {
-  const double G = 0.00000000006674; //6.674e−11
-  position p;
-  double pseudoMagnitude;
-  force f;
-  
-  p.px = bodyFrom.pos.px - bodyOn.pos.px;
-  p.py = bodyFrom.pos.py - bodyOn.pos.py;
-  p.pz = bodyFrom.pos.pz - bodyOn.pos.pz;
-  
-  pseudoMagnitude = -1 * G * bodyOn.mass * bodyFrom.mass
-  
-  f.fx =
+double getMagnitude(vector3D vec) {
+  return sqrt(vec.x*vec.x + vec.y*vec.y + vec.z*vec.z);
 }
 
 
-acceleration getAcceleration(body body, double mass, force force) {
-  //TODO
+/* Given two bodies, the force on body 1 from body 2 will be returned in a force struct
+ * 
+ * @author Jason Vander Woude
+ */
+vector3D getForce(body bodyOn, body bodyFrom) {
+  static const double G = 0.00000000006674; //6.674e−11
+  vector3D p, f;
+  double pseudoMagnitude;
+  
+  p.x = bodyFrom.pos.x - bodyOn.pos.x;
+  p.y = bodyFrom.pos.y - bodyOn.pos.y;
+  p.z = bodyFrom.pos.z - bodyOn.pos.z;
+  
+  pseudoMagnitude = -1 * G * bodyOn.mass * bodyFrom.mass / pow(getMagnitude(p), 3);
+  
+  f.x = pseudoMagnitude * p.x;
+  f.y = pseudoMagnitude * p.y;
+  f.z = pseudoMagnitude * p.z;
+  
+  return f;
+}
+
+
+/* Given a body and the total force vector acting on it the acceleration vector will be returned
+ * 
+ * @author Jason Vander Woude
+ */
+vector3D getAcceleration(body body, vector3D force) {
+  //a = F/m
+  vector3D a;
+  
+  a.x = force.x / body.mass;
+  a.y = force.y / body.mass;
+  a.z = force.z / body.mass;
+  
+  return a;
 }
--- bodies.h
+++ bodies.h
@@ -1,6 +1,38 @@
 #ifndef BODIES_H
 #define BODIES_H
 
+// 3 dimensional position vector
+typedef struct vector3D {
+  double x, y, z;
+} vector3D;
 
+
+// spacial body
+typedef struct body {
+  double density, mass;
+  vector3D pos;
+  vector3D vel;
+} body;
+
+
+/* Given a vector3D, the vector magnitude is returned
+ * 
+ * @author Jason Vander Woude
+ */
+double getMagnitude(vector3D vec);
+
+
+/* Given two bodies, the force on body 1 from body 2 will be returned in a force struct
+ * 
+ * @author Jason Vander Woude
+ */
+vector3D getForce(body bodyOn, body bodyFrom);
+
+
+/* Given a body and the total force vector acting on it the acceleration vector will be returned
+ * 
+ * @author Jason Vander Woude
+ */
+vector3D getAcceleration(body body, vector3D force);
 
 #endif //BODIES_H
