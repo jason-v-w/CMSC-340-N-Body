a8a75ed6e60388411cc8701b073eb6c6ee5bace2
Jason Vander Woude <jasonvwoude@gmail.com>
2017-02-23 23:12:45 -0600

    Update main.c to remove functions now in nbody.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include "nbody.h"
 #include "bodies.h"
 
 
@@ -15,20 +15,6 @@ int              numBodies = 3;
 pthread_mutex_t  mutex;
 body            *bodies;
 vector3D        *forces;
-
-
-// define structs
-typedef struct pair {
-  int a, b;
-} pair;
-
-
-// forward declarations
-pair getNextBodySet();
-int getNextBody();
-void clearForces();
-void* updateForces();
-void* updatePosAndVels();
 
 
 /* Main method
@@ -78,8 +64,6 @@ int main () {
     fflush(stdout);
   }
   
-  
-  
 //   while (1) {
      
     // set all forces to 0
@@ -88,7 +72,7 @@ int main () {
     // initialize all threads
     pthread_t threads[NUM_THREADS];
     for (int t=0; t<NUM_THREADS; ++t) {
-      pthread_create(&(threads[t]), NULL, updateForces, NULL);
+      pthread_create(&(threads[t]), NULL, updateForces, (void*)(&mutex));
     }
 
     // block on thread completion
@@ -98,7 +82,7 @@ int main () {
     
     // initialize all threads
     for (int t=0; t<NUM_THREADS; ++t) {
-      pthread_create(&(threads[t]), NULL, updatePosAndVels, NULL);
+      pthread_create(&(threads[t]), NULL, updatePosAndVels, (void*)(&mutex));
     }
 
     // block on thread completion
@@ -115,115 +99,6 @@ int main () {
     printf("p:<%f, %f, %f>\t\tv:<%f, %f, %f>\n", bodies[i].pos.x, bodies[i].pos.y, bodies[i].pos.z, 
 	                                         bodies[i].vel.x, bodies[i].vel.y, bodies[i].vel.z);
     fflush(stdout);
-  }
-}
-
-
-pair getNextBodySet() {
-    pair p;
-    int static finished = 0;
-  
-    //guard with mutex
-    //TODO: check if mutex can be locked later
-    pthread_mutex_lock(&mutex);
-    
-    if (finished==0) {
-      static int i = 0;
-      static int j = 0;
-      
-      if (j==i) {
-	++i;
-	j=0;
-      }
-      
-      //store return value
-      p.a = i;
-      p.b = j;
-      
-      ++j;
-      
-      if (i==numBodies) {
-	finished = 1;
-      }
-    }
-    
-    if (finished) {
-      p.a = -1;
-      p.b = -1;
-    }
-      
-    //release mutex
-    //TODO: check if mutex can be released earlier
-    pthread_mutex_unlock(&mutex);
-    
-    return p;
-}
-
-
-int getNextBody() {
-  pthread_mutex_lock(&mutex);
-    static int b = -1;
-    static int finished = 0;
-    ++b;
-    if (b==numBodies)
-      finished = 1;
-    if (finished==1)
-      b = -1;
-  pthread_mutex_unlock(&mutex);
-  
-  return b;
-}
-
-
-void clearForces() {
-  for (int i=0; i<numBodies; ++i) {
-    forces[i].x = 0;
-    forces[i].y = 0;
-    forces[i].z = 0;
-  }
-}
-
-
-void *updateForces() {
-  pair p;
-  
-  while (1) {
-    p = getNextBodySet();
-    if (p.a == -1 && p.b == -1) {
-      // kill this thread
-      pthread_exit(NULL);
-    } else {
-      // compute interaction forces between body A and body B
-      vector3D forceOnA = getForce(bodies[p.a], bodies[p.b]);
-      vector3D forceOnB = negateVector3D(forceOnA);
-      
-      // add the respective forces to the running total forces
-      forces[p.a] = vector3DSum(forces[p.a], forceOnA);
-      forces[p.b] = vector3DSum(forces[p.b], forceOnB);
-    }
-  }
-}
-
-
-void *updatePosAndVels() {
-  int x;
-  
-  while (1) {
-    x = getNextBody();
-    if (x == -1) {
-      // kill this thread
-      pthread_exit(NULL);
-    } else {
-      vector3D accel = getAcceleration(bodies[x], forces[x]);
-      // x = x_0 + v*t - 1/2*a*t^2
-      bodies[x].pos = vector3DSum(
-                                  vector3DSum(
-                                              bodies[x].pos, 
-                                              vector3DScale(bodies[x].vel, timeStep)), 
-                                  vector3DScale(accel, 0.5*timeStep*timeStep));
-      // v = v_0 + a*t
-      bodies[x].vel = vector3DSum(bodies[x].vel, vector3DScale(accel, timeStep));
-    }
   }
 }
 
