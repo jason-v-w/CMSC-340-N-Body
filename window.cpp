c28a356ddaf2572e33bf91f6d594789dc62e9c18
Jason Vander Woude <jasonvwoude@gmail.com>
2017-02-23 22:47:52 -0600

    Remove excess code from window.cpp

--- window.cpp
+++ window.cpp
@@ -24,84 +24,24 @@ bool mouse_pressed = false; // Boolean to monitor when mouse is pressed
 
 /* Main method */
 int main() {
+  // set up window
+  sf::RenderWindow window(sf::VideoMode(win_width, win_height), "N-Body", sf::Style::Close|sf::Style::Titlebar);
 
-	sf::RenderWindow window(sf::VideoMode(width, height), 
-	                        "N-Body", 
-	                        sf::Style::Close|sf::Style::Titlebar);
-
-	// Create sprite to hold image data
-	sf::Sprite image;
-
-	sf::RectangleShape selector(sf::Vector2f(select_width, select_height));
-	selector.setOutlineThickness(1);
-	selector.setOutlineColor(sf::Color(128, 0, 128));
-	selector.setFillColor(sf::Color(0, 0, 0, 0));
-
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			switch (event.type) {
-				
-				// Window closed
-				case sf::Event::Closed:
-					window.close();
-					break;
-
-				// Mouse Button Pressed
-				case sf::Event::MouseButtonPressed:
-					switch (event.mouseButton.button) {
-						
-						// Left Button Pressed
-						case sf::Mouse::Left:
-							// TODO
-							break;
-							
-						// Right Button Pressed
-						case sf::Mouse::Right:
-							// TODO
-							break;
-
-						// Default event.mouseButton.button
-						default:
-							// Won't be processed
-							break;
-					} //END: event.mouseButton.button
-					break;
-
-				// Mouse Button Released
-				case sf::Event::MouseButtonReleased:
-					switch (event.mouseButton.button) {
-						
-						// Left Button Pressed
-						case sf::Mouse::Left:
-							// TODO
-							break;
-							
-						// Right Button Pressed
-						case sf::Mouse::Right:
-							// TODO
-							break;
-
-						// Default event.mouseButton.button
-						default:
-							// Won't be processed
-							break;
-					} //END: event.mouseButton.button
-					break;
-
-				// Default event.type
-				default:
-					// Won't process other events
-					break;
-					
-			} //END: switch (event.type)
-		} //END: while (window.pollEvent(event))
-
-		// Do display operations
-		window.clear();
-		window.display();
-		
-	} //END: while (window.isOpen())
- 
-	return 0;
+  while (1) {
+    
+    
+    
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+	window.close();
+      }
+    }
+    
+    // Do display operations
+    window.clear();
+    window.display();
+  }
+    
+  return 0;
 }
